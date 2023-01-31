ebe7f014a4171963a11b4a5bc79d1198275f8c51
Joshua Ukiru <103338142+Tumaini98@users.noreply.github.com>
2023-01-31 17:38:53 -0600

    added user form and pages folder

---
+++ UpRise.Starter.Core/UpRise.Models/Requests/FAQ/FaqAddRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpRise.Models.Requests.FAQ
+{
+    public class FaqAddRequest
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public int CategoryId { get; set; }
+        public int SortOrder { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public int CreatedBy { get; set; }
+        public int ModifiedBy { get; set; }
+    }
+}
---
+++ UpRise.Starter.Core/UpRise.Services/FAQ/FAQService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpRise.Data.Interfaces;
+
+namespace UpRise.Services.FAQ
+{
+    public class FAQService
+    {
+        IDataProvider _Data;
+        public FAQService(IDataProvider data)
+        {
+            _Data = data;
+
+        }
+
+
+        public FAQ getAll()
+        {
+            FAQ faq = new FAQ();
+
+            return faq;
+
+        }
+    }
+
+}
